7ce9ecdd0efef88bd1ce2747923b4030183b3c5f
Khristine Sagum <tinsagum11@gmail.com>
2022-02-17 03:21:23 +0300

    first commit

---
+++ Lessons.js
@@ -0,0 +1,83 @@
+let lessons = [
+    {
+        "id": 1001,
+        "subject": "Math",
+        "Location": "London",
+        "Price": 100,
+        "Spaces": 5,
+        "image": "Static/Math.PNG"
+    },
+    {
+        "id": 1002,
+        "subject": "English",
+        "Location": "York",
+        "Price": 80,
+        "Spaces": 5,
+        "image": "Static/English.png"
+    },
+    {
+        "id": 1003,
+        "subject": "Music",
+        "Location": "Bristol",
+        "Price": 90,
+        "Spaces": 5,
+        "image": "Static/Music.jpg"
+    },
+    {
+        "id": 1004,
+        "subject": "Physical Education",
+        "Location": "Manchester",
+        "Price": 80,
+        "Spaces": 5,
+        "image": "Static/PE.jpg"
+    },
+    {
+        "id": 1005,
+        "subject": "Arts",
+        "Location": "Birmingham",
+        "Price": 90,
+        "Spaces": 5,
+        "image": "Static/Arts.jpg"
+    },
+    {
+        "id": 1006,
+        "subject": "History",
+        "Location": "Liverpool",
+        "Price": 80,
+        "Spaces": 5,
+        "image": "Static/History.png"
+    },
+    {
+        "id": 1007,
+        "subject": "Geography",
+        "Location": "Newcastle",
+        "Price": 100,
+        "Spaces": 5,
+        "image": "Static/Geography.png"
+    },
+    {
+        "id": 1008,
+        "subject": "Computer",
+        "Location": "Leeds",
+        "Price": 80,
+        "Spaces": 5,
+        "image": "Static/Computer.png"
+    },
+    {
+        "id": 1009,
+        "subject": "Literacy",
+        "Location": "Cambridge",
+        "Price": 74,
+        "Spaces": 5,
+        "image": "Static/Literacy.png"
+    },
+    {
+        "id": 1010,
+        "subject": "Science",
+        "Location": "Oxford",
+        "Price": 75,
+        "Spaces": 5,
+        "image": "Static/Science.png"
+    }
+
+]
